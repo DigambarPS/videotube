923f2c44444ee79be476186b5e7d81eb3ab63644
= <=>
2026-06-14 01:53:42 +0530

    setup and middlewares for file uploads

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadToCloudinary = async (localPath) =>{
+    try{
+        if(!localPath) return null
+
+        const response = await cloudinary.uploader.upload(localPath,{
+            resource_type: "auto"
+        })
+        console.log("File uploaded to cloudinary: ",response)
+        return response
+    }catch(err){
+        fs.unlinkSync(localPath)
+        console.log("Error in file upload to cloudinary: ",err)
+        return null
+    }
+}
+
+export {uploadToCloudinary}
