cc787ecaa485c4e2d67a11ca435b726b579b0527
= <=>
2026-06-14 00:57:59 +0530

    bug fix in ApiError class

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error{
         statusCode,
         message = 'Something went wrong',
         errors = [],
-        statck = ""
+        stack = ""
     )
     {
         super(message)
@@ -12,9 +12,9 @@ class ApiError extends Error{
         this.message = message
         this.success = false
         this.errors = errors
-        if(statck)
+        if(stack)
         {
-            this.stack = statck
+            this.stack = stack
         }else{
             Error.captureStackTrace(this, this.constructor)
         }
