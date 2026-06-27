f8b6ef18785362bcf4c9cd2764968632688c1fc2
= <=>
2026-06-28 00:46:52 +0530

    comments controller additional changes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,175 +1,179 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {Video} from "../models/video.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
-    //validation
-    if(!videoId)
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+  //validation
+  if (!videoId) {
+    throw new ApiError(404, "videoId is mandatory");
+  }
+
+  if (!page || page < 1) {
+    throw new ApiError(404, "Page value is mandatory");
+  }
+
+  if (!limit) {
+    throw new ApiError(404, "Limit value is mandatory");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "invalid videoId");
+  }
+
+  const options = {
+    page: page,
+    limit: limit   
+  };
+  
+  const videoComments = await Video.aggregatePaginate(Video.aggregate([
     {
-        throw new ApiError(404, 'videoId is mandatory')
-    }
-
-    if(!page || page<1)
+      $match: {
+        _id: videoId,
+      },
+    },
     {
-        throw new ApiError(404, 'Page value is mandatory')
-    }
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "_id",
+              foreignField: "owner",
+              as: "commentors",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },            
+          },
+        ],
+      },
+    },
+  ]),options);
 
-    if(!limit)
-    {
-        throw new ApiError(404, "Limit value is mandatory")
-    }
+  if(!videoComments)
+  {
+    throw new ApiError(500, "Something went wrong while fetching comments of video")
+  }
 
-    const video = await Video.findById(videoId)
-
-    if(!video)
-    {
-        throw new ApiError(404, "invalid videoId")
-    }
-
-    const videoComments = await Comment.aggregate([
-        {
-            $match:{
-                video: videoId
-            }
-        },
-        {
-            $lookup:{
-                from:'comments',
-                localField:"_id",
-                foreignField:"video",
-                as: "comments"
-            }
-        },
-        {
-            $lookup:{
-                from:"comments"
-            }
-        }
-    ])
-    //pipeline
-        //match on video id
-        //get data commenter name and comment content, 
-        // like count of that comment, 
-        // if I have liked that comment flag
-        //$newfields for above and $project
-    //then apply pagination and limit and return data to frontend
-
-})
+  return res
+  .status(200)
+  .json(new ApiResponse(200, videoComments, "Video comments fetched successfully"))
+  
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-    const { videoId } = req.params
-    const { content } = req.body
+  // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
 
-    if(!videoId)
-    {
-        throw new ApiError(400, "videoId is mandatory")
-    }
+  if (!videoId) {
+    throw new ApiError(400, "videoId is mandatory");
+  }
 
-    if(!content)
-    {
-        throw new ApiError(400, "content is mandatory")
-    }
+  if (!content) {
+    throw new ApiError(400, "content is mandatory");
+  }
 
-    const video = await Video.findById(videoId)
+  const video = await Video.findById(videoId);
 
-    if(!video)
-    {
-        throw new ApiError(404, "videoId is invalid")
-    }
+  if (!video) {
+    throw new ApiError(404, "videoId is invalid");
+  }
 
-    const comment = await Comment.create({content:content, owner:req.user?._id, video:videoId})
+  const comment = await Comment.create({
+    content: content,
+    owner: req.user?._id,
+    video: videoId,
+  });
 
-    if(!comment)
-    {
-        throw new ApiError(500, "Something went wrong, comment not stored")
-    }
+  if (!comment) {
+    throw new ApiError(500, "Something went wrong, comment not stored");
+  }
 
-    return res
+  return res
     .status(200)
-    .json( new ApiResponse(200, comment, "Comment added successfully"))
-})
+    .json(new ApiResponse(200, comment, "Comment added successfully"));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-    const { commentId } = req.params
-    const { content } = req.body
+  // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
 
-    if(!commentId)
+  if (!commentId) {
+    throw new ApiError(404, "commentId is mandatory");
+  }
+
+  if (!content) {
+    throw new ApiError(404, "content is mandatory");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(400, "commentId is invalid");
+  }
+
+  const comment = await Comment.findByIdAndUpdate(
+    commentId,
     {
-        throw new ApiError(404, "commentId is mandatory")
-    }
-
-    if(!content)
+      $set: { content: content },
+    },
     {
-        throw new ApiError(404, "content is mandatory")
+      new: true,
     }
+  );
 
-    const comment = await Comment.findById(commentId)
+  if (!comment) {
+    throw new ApiError(500, "Something went wrong while updating comment");
+  }
 
-    if(!comment)
-    {
-        throw new ApiError(400, "commentId is invalid")
-    }
-
-    const comment = await Comment.findByIdAndUpdate(
-        commentId, 
-        {
-            $set:{content:content}
-        },
-        {
-            new: true
-        }
-    )
-
-    if(!comment)
-    {
-        throw new ApiError(500, "Something went wrong while updating comment")
-    }
-
-    return res
+  return res
     .status(200)
-    .json( new ApiResponse(200, comment, "Comment updated successfully"))
-
-})
+    .json(new ApiResponse(200, comment, "Comment updated successfully"));
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-    const { commentId } = req.params
+  // TODO: delete a comment
+  const { commentId } = req.params;
 
-    if(!commentId)
-    {
-        throw new ApiError(404, "commentId is mandatory")
-    }
+  if (!commentId) {
+    throw new ApiError(404, "commentId is mandatory");
+  }
 
-    const comment = await Comment.findById(commentId)
+  const comment = await Comment.findById(commentId);
 
-    if(!comment)
-    {
-        throw new ApiError(400, "commentId is invalid")
-    }
+  if (!comment) {
+    throw new ApiError(400, "commentId is invalid");
+  }
 
-    const comment = await Comment.findByIdAndDelete(commentId)
+  const comment = await Comment.findByIdAndDelete(commentId);
 
-    if(!comment)
-    {
-        throw new ApiError(500, "Something went wrong while deleting comment")
-    }
+  if (!comment) {
+    throw new ApiError(500, "Something went wrong while deleting comment");
+  }
 
-    return res
+  return res
     .status(200)
-    .json( new ApiResponse(200, comment, "Comment deleted successfully"))
-})
+    .json(new ApiResponse(200, comment, "Comment deleted successfully"));
+});
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-    deleteComment
-    }
+export { getVideoComments, addComment, updateComment, deleteComment };
