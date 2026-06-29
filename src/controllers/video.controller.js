9e0dba8928287c5132516e1b99870d198671deb5
= <=>
2026-06-30 00:57:26 +0530

    video controller inprogress

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -15,16 +15,98 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    
+    if(!title || !description)
+    {
+        throw new ApiError(401, 'title and description are mandatory fields')
+    }
+
+    const getLocalVideoPath = req.files[0]?.videoFile[0]?.path
+    const getLocalThumbnailPath = req.files[0]?.thumbnail[0]?.path
+
+    if(!getLocalVideoPath)
+    {
+        throw new ApiError(401, 'Videofile is mandatory')
+    }
+
+    if(!getLocalThumbnailPath)
+    {
+        throw new ApiError(401, 'thumbnail is mandatory')
+    }
+
+    const videoUpload = await uploadToCloudinary(getLocalVideoPath)
+
+    if(!videoUpload)
+    {
+        throw new ApiError(500, 'Video upload to cloudinary failed')
+    }
+
+    const thumbnailUpload = await uploadToCloudinary(getLocalThumbnailPath)
+
+    if(!thumbnailUpload)
+    {
+        throw new ApiError(500, 'Thumbnail upload to cloudinary failed')
+    }
+
+    const video = Video.create({
+        videoFile:videoUpload.secure_url,
+        title: title,
+        description: description,
+        thumbnail: thumbnailUpload.secure_url,
+        duration: videoUpload.duration,
+        isPublished: true,
+        views:0,
+        owner: req.user?._id
+    })
+
+    if(!video)
+    {
+        throw new ApiError(500, 'Something went wrong creating video')
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, 'Video uploaded successfully')
+    )
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    if(!videoId)
+    {
+        throw new ApiError(401, 'videoId is mandatory')
+    }
+
+    const video = Video.findById(videoId)
+
+    if(!video)
+    {
+        throw new ApiError(403, 'video not found')
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, video, 'video fetched successfully')
+    )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    if(!videoId)
+    {
+        throw new ApiError(401, 'videoId is mandatory')
+    }
+
+    const video = await Video.countDocuments({_id:videoId})
+
+    if(!video)
+    {
+        throw new ApiError(403, 'video not found')
+    }
 
 })
 
--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,6 +1,5 @@
 import mongoose, { Schema } from "mongoose"
 
-
 const likeSchema = new Schema({
     likedBy:{
         type: Schema.Types.ObjectId,
